19a86f491c584e372e510cbf0c3d1f7534ed6374
chanki <chanki.kim89@gmail.com>
2022-04-29 16:32:48 +0900

    슬라이드쇼수정

--- src/pages/SlideShow/index.js
+++ src/pages/SlideShow/index.js
@@ -17,7 +17,7 @@ const SlideShow = ({ menus }) => {
                 <div className="empty">슬라이드쇼를 위한 사진리스트가 업습니다.</div>
             ) : (
                 <div className="slide-container slide-show">
-                    <Swiper modules={[Autoplay, EffectFade]} autoplay={{ delay: 2000 }} effect={"fade"}>
+                    <Swiper modules={[Autoplay, EffectFade]} autoplay={{ delay: 2000 }} effect={"fade"} allowTouchMove={false}>
                         {imgLists && imgLists.map((list, idx) => <SwiperSlide key={idx} style={{ backgroundImage: "url(" + list + ")" }}></SwiperSlide>)}
                     </Swiper>
                 </div>
